a9cc05603497c74233d1eab01a14a2894d6d9dc6
Samuel Young <samueldy@umich.edu>
2023-10-16 13:26:42 -0400

    Make scripts more secure; add script for Thunar inside NX session

--- start-scripts/start-vnc-docker-perlmutter.sh
+++ start-scripts/start-vnc-docker-perlmutter.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Shortcut to start Docker container via Podman-HPC on NERSC
 
-    # -e 'VNC_PORT=57868' \
-    # --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-    # --net host \
-OUTER_VNC_PORT=24835 # Set to somethign that no one else is using.
+OUTER_VNC_PORT=$RANDOM # Set to something that no one else is using.
+USERNAME=$(whoami)
+export VNC_PW="$(cat /dev/random | tr -dc "[0-9A-Za-z]" | head -c 20)"
+
+cat << EOF
+Setting up VNC server on node's port $OUTER_VNC_PORT. Forward a local port on your local computer to this port to connect to VNC.
+Username is $USERNAME.
+The VNC password is: $VNC_PW
+Don't share it with others!
+EOF
 
 podman-hpc run -it --userns keep-id:uid=1000,gid=1000 -p $OUTER_VNC_PORT:5901 \
     -e DISPLAY \
-    -v /global/u2/s/samueldy:/home/samueldy \
+    -e VNC_PW \
+    -v "$(pwd -P $HOME):/home/$USERNAME" \
     -v $(readlink -f ~/.config):/headless/.config \
     -v $(readlink -f ~/.themes):/headless/.themes \
     -v $(readlink -f ~/.icons):/headless/.icons \
     -v /global:/global \
     -v /pscratch:/pscratch \
-    registry.nersc.gov/m4357/samueldy-vnc-docker-ubuntu $@ 
+    docker://docker.io/samueldy/vnc-docker-debian-xfce4 $@ 
